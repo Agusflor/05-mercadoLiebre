4b5ec025391d69d9cbb2d1184796e7309c394cbc
Agusflor <agusflormamani@gmail.com>
2022-09-02 21:25:39 -0300

    Tp forms

--- app.js
+++ app.js
@@ -8,5 +8,6 @@ app.use(express.static('public'));
 /* rutas */
 app.get('/',(req,res)=> res.sendFile(path.resolve(__dirname,'views', 'home.html')));
 app.get('/register',(req,res)=> res.sendFile(path.resolve(__dirname,'views', 'register.html')));
+app.get('/login',(req,res)=> res.sendFile(path.resolve(__dirname,'views', 'login.html')));
 /*server */
 app.listen(port,()=> console.log(`server running https://localhost:${port}`));
